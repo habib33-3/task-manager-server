6b49c078cef5a26497df953ce7e26faeef63e0ae
Habibur Rahman <habib.swi16@gmail.com>
2023-12-22 18:03:47 +0600

    add task

--- index.js
+++ index.js
@@ -32,11 +32,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const db = client.db("task-manager");
+    const taskCollection = db.collection("tasks");
+
+   
+
+    app.post("/api/v1/addTask", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+   
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
