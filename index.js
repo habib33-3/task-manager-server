1c1ceb4728389c8a3e8b2b3c6ab96bc9657da033
Habibur Rahman <habib.swi16@gmail.com>
2023-12-23 13:56:17 +0600

    delete task

--- index.js
+++ index.js
@@ -98,6 +98,16 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/api/v1/task/delete/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+
+      const result = await taskCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
